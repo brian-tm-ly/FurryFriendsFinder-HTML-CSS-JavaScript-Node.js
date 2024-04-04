e6e9e80918767b0973be48ec40c72337bb79579b
boredboy97 <ly.brian@hotmail.ca>
2024-04-04 15:11:43 -0400

    added exercise folders 1-3

---
+++ Exercise1/index.js
@@ -0,0 +1,113 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { type } from "os";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Question 1
+
+//A) Function that takes a positive integer and returns sum of numbers 1 to N
+function findSummation(n) {
+  let sum = 0;
+  //check if n is a number and positive
+  if (typeof n !== "number" || n <= 0) {
+    return false;
+  } else {
+    for (let i = 0; i < n; i++) {
+      sum += i; //add i to sum
+    }
+  }
+  return sum;
+}
+
+//B) Function that capitalizes the first and last letter of each word in a string
+function uppercaseFirstandLast(str) {
+  const words = str.split(" "); //split string into words
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let firstLetter = word.charAt(0).toUpperCase(); //get first letter and capitalize
+    let lastLetter = word.charAt(word.length - 1).toUpperCase(); //get last letter and capitalize
+    let middle = word.slice(1, word.length - 1); //get middle of word
+    words[i] = firstLetter + middle + lastLetter; //concatenate first, middle and last letter
+  }
+  return words.join(" "); //join words back to string
+}
+
+//C) Function that returns the average and median of an array of numbers
+function findAverageAndMedian(numbers) {
+  const sortedNumbers = numbers.sort(); //sort numbers
+  let sum = sortedNumbers.reduce((acc, current) => acc + current);
+  let length = sortedNumbers.length;
+  let median;
+  if (length % 2 === 0) {
+    //check if length of array is even
+    median = (sortedNumbers[length / 2 - 1] + sortedNumbers[length / 2]) / 2;
+  } else {
+    median = sortedNumbers[Math.floor(length / 2)];
+  }
+  const average = sum / length;
+  return { average, median };
+}
+
+//D) Function that returns the first 4-digit number in the string, otherwise false
+function find4Digits(str) {
+  const digitsPattern = /\d{4}/; //regex pattern for 4 digits
+  const match = str.match(digitsPattern); //match pattern in string
+  if (match) {
+    return parseFloat(match);
+  } else {
+    return false;
+  }
+}
+
+//Route handling
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/findsummation", (req, res) => {
+  const n = parseInt(req.body.integer);
+  const sumResult = findSummation(n);
+  res.send(`<b>Sum of numbers from 1 to ${n}: </b>` + `${sumResult}`);
+});
+
+app.post("/uppercasefirstandlast", (req, res) => {
+  const str = req.body.string;
+  const result = uppercaseFirstandLast(str);
+  res.send(`<b>String with first and last letter capitalized: </b>` + result);
+});
+
+app.post("/findaverageandmedian", (req, res) => {
+  //convert string of numbers to array of numbers
+  const numbers = req.body.numbers.split(",").map(Number);
+  const result = findAverageAndMedian(numbers);
+  res.send(
+    `<b>Average: </b>` +
+      `${result.average}` +
+      `<br>` +
+      `<b>Median: </b>` +
+      `${result.median}`
+  );
+});
+
+app.post("/find4digits", (req, res) => {
+  const str = req.body.str;
+  const result = find4Digits(str);
+  result
+    ? res.send(`<b>First 4-digit number: </b>` + result)
+    : res.send("No 4-digit number found.");
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Server running on port ${port}`);
+});
---
+++ Exercise2/numOfVisits.js
@@ -0,0 +1,63 @@
+import express from "express"; //Require the express module
+import cookieParser from "cookie-parser"; //Require the cookie-parser module
+import moment from "moment-timezone"; //Require the moment-timezone module
+const app = express();
+const port = 3000;
+const cookieName = "numOfVisits";
+
+//Using cookie-parser middleware
+app.use(cookieParser());
+
+//Middleware to count the number of visits
+app.use((req, res, next) => {
+  //If the cookie is not set, set it to 0
+  let visits = parseInt(req.cookies[cookieName]) || 0;
+  visits++; //Increment the number of visits
+  //Set the cookie to the new value and set the expiration date to 1 day
+  res.cookie(cookieName, visits, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+  });
+  //Store the number of visits in the request object
+  req.visits = visits;
+  //Continue with the request
+  next();
+});
+
+//Route to handle the root URL
+app.get("/", (req, res) => {
+  //If it is the first time that the user is visiting the webpage
+  if (req.visits == 1) {
+    res.send("Welcome to my webpage! It is the first time that you are here.");
+    //If the user has visited the webpage before
+  } else {
+    const lastVisit = moment().tz("EST").format("ddd MMM DD HH:mm:ss z YYYY");
+    if (req.visits == 2) {
+      res.send(
+        `Hello, this is the ${req.visits}nd time that you are visiting my webpage.<br>
+        <b>Last time you visited my webpage was on: ${lastVisit}<b>`
+      );
+    } else if (req.visits == 3) {
+      res.send(
+        `Hello, this is the ${req.visits}rd time that you are visiting my webpage.<br>
+        <b>Last time you visited my webpage was on: ${lastVisit}<b>`
+      );
+    } else {
+      res.send(
+        `Hello, this is the ${req.visits}th time that you are visiting my webpage.<br>
+      <b>Last time you visited my webpage was on: ${lastVisit}<b>`
+      );
+    }
+  }
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).send("404 Not Found");
+});
+
+//Error handler
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`Server is running on port ${port}`);
+});
